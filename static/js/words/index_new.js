de33255a73419e81961f0ae51661582ac7643694
Guy <guylifshitz@gmail.com>
2022-04-24 21:39:55 +0300

    - edit popup for words index - add context menu on each word line

--- static/js/words/index_new.js
+++ static/js/words/index_new.js
@@ -46,8 +46,7 @@ async function clicked_update(word, rating) {
 async function select_by_filter(filter_rating) {
   const db = await get_db();
 
-  // do we need to change this to a blocking for?
-  words.forEach(async (word) => {
+  for (const word of words) {
     res = await db.get("lemmas", word["word"]);
     if (res) {
       word_rating = res.rating;
@@ -57,14 +56,14 @@ async function select_by_filter(filter_rating) {
           .change();
       }
     }
-  });
+  }
+  hide_unselected();
 }
 
 async function deselect_by_filter(filter_rating) {
   const db = await get_db();
 
-  // do we need to change this to a blocking for?
-  words.forEach(async (word) => {
+  for (const word of words) {
     res = await db.get("lemmas", word["word"]);
     if (res) {
       word_rating = res.rating;
@@ -74,15 +73,18 @@ async function deselect_by_filter(filter_rating) {
           .change();
       }
     }
-  });
+  }
+  hide_unselected();
 }
 
 function clear_selection() {
   $(".select-word-checkbox").prop("checked", false).change();
+  hide_unselected();
 }
 
 function select_all() {
   $(".select-word-checkbox").prop("checked", true).change();
+  hide_unselected();
 }
 
 async function import_word_ratings() {
@@ -160,55 +162,6 @@ async function submit_articles_form(form) {
   form.submit();
 }
 
-async function examples_word(practice_word, token) {
-  const db = await get_db();
-
-  var practice_words = [practice_word];
-  var known_words2 = [];
-  known_words2 = await db.getAllKeys("lemmas");
-  known_words2 = known_words2.filter(function (value, index, arr) {
-    return !practice_words.includes(value);
-  });
-
-  var form = document.createElement("form");
-
-  form.setAttribute("method", "post");
-  form.setAttribute("action", "articles/index");
-
-  var token_input = document.createElement("input");
-  token_input.setAttribute("type", "hidden");
-  token_input.setAttribute("name", "csrfmiddlewaretoken");
-  token_input.setAttribute("value", token);
-  form.appendChild(token_input);
-
-  var practice_words_input = document.createElement("input");
-  practice_words_input.setAttribute("type", "hidden");
-  practice_words_input.setAttribute("name", "practice_words");
-  practice_words_input.setAttribute("value", JSON.stringify(practice_words));
-  form.appendChild(practice_words_input);
-
-  var known_words_input = document.createElement("input");
-  known_words_input.setAttribute("type", "hidden");
-  known_words_input.setAttribute("name", "known_words");
-  known_words_input.setAttribute("value", JSON.stringify(known_words2));
-  form.appendChild(known_words_input);
-
-  var start_date_input = document.createElement("input");
-  start_date_input.setAttribute("type", "hidden");
-  start_date_input.setAttribute("name", "start_date");
-  start_date_input.setAttribute("value", "2020-01-01");
-  form.appendChild(start_date_input);
-
-  var sort_by_practice_input = document.createElement("input");
-  sort_by_practice_input.setAttribute("type", "hidden");
-  sort_by_practice_input.setAttribute("name", "sort_by_practice_only");
-  sort_by_practice_input.setAttribute("value", "NOTESET");
-  form.appendChild(sort_by_practice_input);
-
-  document.body.appendChild(form);
-  form.submit();
-}
-
 async function set_articles_submit_json(practice_words) {
   const db = await get_db();
 
@@ -257,22 +210,19 @@ async function get_known_words() {
 
   const db = await get_db();
 
-  // do we need to change this to a blocking for?
-  words.forEach(async (word) => {
-    for (const word of words) {
-      res = await db.get("lemmas", word["word"]);
-      if (res) {
-        if (practice_words.includes(res[word["word"]])) {
-          rating_text = "PRACTICE";
-        } else if (res["rating"] == 5) {
-          rating_text = "KNOWN";
-        } else {
-          rating_text = "SEEN";
-        }
-        output.push([word["word"], rating_text]);
+  for (const word of words) {
+    res = await db.get("lemmas", word["word"]);
+    if (res) {
+      if (practice_words.includes(res[word["word"]])) {
+        rating_text = "PRACTICE";
+      } else if (res["rating"] == 5) {
+        rating_text = "KNOWN";
+      } else {
+        rating_text = "SEEN";
       }
+      output.push([word["word"], rating_text]);
     }
-  });
+  }
 }
 
 function sendData(url, parameters) {
@@ -383,4 +333,178 @@ function hide_popups() {
 function show_articles_popup() {
   $("#articles-popup").show();
   $("#words-selected-count").text($(".select-word-checkbox:checked").length);
+}
+
+function hide_unselected() {
+  if ($("#filter-unselected").prop("checked")) {
+    $(".select-word-checkbox:not(checked)").parent().parent().parent().hide();
+    $(".select-word-checkbox:checked").parent().parent().parent().show();
+    // $("#filter-unselected").attr("onclick", "show_unselected()");
+  } else {
+    $(".select-word-checkbox").parent().parent().parent().show();
+    $("#filter-unselected").attr("onclick", "hide_unselected()");
+  }
+}
+
+function show_edit_popup(word) {
+  $("#popup-word").text(word.word);
+  $("#root").text(word.root);
+  $("#existing-roots").text(word.user_roots);
+  $("#translation").text(word.translation);
+  $("#existing-translations").text(word.user_translations);
+  $(".popup").show();
+
+  var datalist = $("#existing_translations_list");
+  datalist.empty();
+  word.user_translations.forEach(function (t) {
+    datalist.append("<option value='" + t + "'>");
+  });
+
+  var datalist = $("#existing_roots_list");
+  datalist.empty();
+  word.user_roots.forEach(function (r) {
+    datalist.append("<option value='" + r + "'>");
+  });
+}
+
+function hide_edit_popup() {
+  $(".popup").hide();
+}
+
+async function examples_word(practice_word, token) {
+  const db = await get_db();
+
+  var practice_words = [practice_word];
+  var known_words2 = [];
+  known_words2 = await db.getAllKeys("lemmas");
+  known_words2 = known_words2.filter(function (value, index, arr) {
+    return !practice_words.includes(value);
+  });
+
+  var form = document.createElement("form");
+
+  form.setAttribute("method", "post");
+  form.setAttribute("action", "articles/index");
+
+  var token_input = document.createElement("input");
+  token_input.setAttribute("type", "hidden");
+  token_input.setAttribute("name", "csrfmiddlewaretoken");
+  token_input.setAttribute("value", token);
+  form.appendChild(token_input);
+
+  var practice_words_input = document.createElement("input");
+  practice_words_input.setAttribute("type", "hidden");
+  practice_words_input.setAttribute("name", "practice_words");
+  practice_words_input.setAttribute("value", JSON.stringify(practice_words));
+  form.appendChild(practice_words_input);
+
+  var known_words_input = document.createElement("input");
+  known_words_input.setAttribute("type", "hidden");
+  known_words_input.setAttribute("name", "known_words");
+  known_words_input.setAttribute("value", JSON.stringify(known_words2));
+  form.appendChild(known_words_input);
+
+  var start_date_input = document.createElement("input");
+  start_date_input.setAttribute("type", "hidden");
+  start_date_input.setAttribute("name", "start_date");
+  start_date_input.setAttribute("value", "2020-01-01");
+  form.appendChild(start_date_input);
+
+  var sort_by_practice_input = document.createElement("input");
+  sort_by_practice_input.setAttribute("type", "hidden");
+  sort_by_practice_input.setAttribute("name", "sort_by_practice_only");
+  sort_by_practice_input.setAttribute("value", "NOTESET");
+  form.appendChild(sort_by_practice_input);
+
+  document.body.appendChild(form);
+  form.submit();
+}
+
+function show_context_menu(element) {
+  var rect = element.getBoundingClientRect();
+  console.log(rect);
+  $("#context-menu").show();
+  $("#context-menu").css("left", element.offsetLeft + 15);
+  $("#context-menu").css("top", element.offsetTop + 5);
+  $("#context-menu").attr("word", $(element).attr("word"));
+}
+
+window.onclick = function (event) {
+  if (
+    !(
+      event.target.matches("#context-menu") ||
+      event.target.matches(".show_context_menu")
+    )
+  ) {
+    console.log("hide ");
+    $("#context-menu").hide();
+  }
+};
+
+function context_menu_open_definition(element, language) {
+  window.open(
+    "https://en.wiktionary.org/wiki/" +
+      $("#context-menu").attr("word") +
+      "#" +
+      language
+  );
+}
+
+function context_menu_examples_word(element, token) {
+  examples_word($("#context-menu").attr("word"), token);
+}
+
+function context_menu_edit_word() {
+  show_edit_popup(words_to_show_dict[$("#context-menu").attr("word")]);
+}
+
+function update_translation() {
+  let new_translation = document.getElementById("new_translation").value;
+  word = $("#popup-word").text();
+
+  data = {
+    new_user_translation: new_translation,
+  };
+  $.ajax({
+    type: "PATCH",
+    url: "http://localhost:8001/api/words/" + word + "/",
+    data: JSON.stringify(data),
+    processData: false,
+    contentType: "application/json",
+  })
+    .done(function () {
+      alert("success");
+    })
+    .fail(function () {
+      alert("error");
+    })
+    .always(function () {
+      // alert("complete");
+    });
+}
+
+function update_root() {
+  let new_root = document.getElementById("new_root").value;
+  word = $("#popup-word").text();
+
+  data = {
+    new_user_root: new_root,
+  };
+  $.ajax({
+    type: "PATCH",
+    url: "http://localhost:8001/api/words/" + word + "/",
+    data: JSON.stringify(data),
+    processData: false,
+    contentType: "application/json",
+  })
+    .done(function () {
+      alert("success");
+      $("#" + word + "-root").text(new_root);
+    })
+    .fail(function () {
+      alert("error");
+    })
+    .always(function () {
+      // alert("complete");
+    });
 }
