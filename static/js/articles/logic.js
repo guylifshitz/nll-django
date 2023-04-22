22ed1463e00b9c369252dbd2f7e74e2d8864fef1
Guy <guylifshitz@gmail.com>
2023-04-22 16:10:43 +0300

    - remove old code for ratings on articles - fix clickon for articles page icons

--- static/js/articles/logic.js
+++ static/js/articles/logic.js
@@ -8,6 +8,89 @@ function position_tooltips() {
   });
 }
 
+function show_full_translation(element) {
+  $(element).parent().parent().find(".title_translation").toggle();
+  position_tooltips();
+}
+
+function speak_title_helper(element) {
+  div = $(element).parent().parent();
+  speak_title(div);
+}
+function speak_title(speakElement) {
+  bigstring = "";
+  speakElement
+    .find(".title")
+    .find(".word")
+    .each(function (index, item) {
+      console.log(item);
+      bigstring += $(item).text() + " ";
+    });
+
+  var msg = new SpeechSynthesisUtterance();
+  msg.text = bigstring;
+  msg.lang = speech_voice;
+  msg.rate = 0.7;
+  window.speechSynthesis.cancel();
+  window.speechSynthesis.speak(msg);
+
+  msg.onboundary = function (event) {
+    console.log(event.charIndex);
+    console.log(getWordAt(bigstring, event.charIndex));
+
+    word_idx = getWordIndexAtCharIndex(bigstring, event.charIndex);
+    idOfChild = "word_" + word_idx;
+    speakElement.find("#" + idOfChild).css("color", "blue");
+  };
+
+  msg.onend = function (event) {
+    $(".word").css("color", "black");
+    console.log("DONE");
+  };
+}
+
+function speak(text) {
+  var msg = new SpeechSynthesisUtterance();
+  msg.text = text;
+  msg.lang = speech_voice;
+  msg.rate = 0.5;
+  window.speechSynthesis.cancel();
+  window.speechSynthesis.speak(msg);
+}
+
+// Get the word of a string given the string and the index
+function getWordAt(str, pos) {
+  // Perform type conversions.
+  str = String(str);
+  pos = Number(pos) >>> 0;
+
+  // Search for the word's beginning and end.
+  var left = str.slice(0, pos + 1).search(/\S+$/),
+    right = str.slice(pos).search(/\s/);
+
+  // The last word in the string is a special case.
+  if (right < 0) {
+    return str.slice(left);
+  }
+  // Return the word, using the located bounds to extract it from the string.
+  return str.slice(left, right + pos);
+}
+
+function getWordIndexAtCharIndex(str, pos) {
+  str = String(str);
+  pos = Number(pos) >>> 0;
+
+  text_so_far = str.slice(0, pos);
+  num_words_so_far = text_so_far.split(" ").length;
+
+  return num_words_so_far;
+}
+
+function show_partial_translation(element) {
+  $(element).parent().parent().find(".title_mix").toggle();
+  position_tooltips();
+}
+
 $(document).ready(function () {
   function google_translate_word() {
     html_txt = $(event.target).attr("original_txt");
@@ -21,95 +104,6 @@ $(document).ready(function () {
       alert(html_txt + ":  " + data.data.translations[0].translatedText);
     });
   }
-
-  function show_full_translation(event) {
-    $(event.target).parent().parent().find(".title_translation").toggle();
-    position_tooltips();
-  }
-
-  // Get the word of a string given the string and the index
-  function getWordAt(str, pos) {
-    // Perform type conversions.
-    str = String(str);
-    pos = Number(pos) >>> 0;
-
-    // Search for the word's beginning and end.
-    var left = str.slice(0, pos + 1).search(/\S+$/),
-      right = str.slice(pos).search(/\s/);
-
-    // The last word in the string is a special case.
-    if (right < 0) {
-      return str.slice(left);
-    }
-    // Return the word, using the located bounds to extract it from the string.
-    return str.slice(left, right + pos);
-  }
-
-  function getWordIndexAtCharIndex(str, pos) {
-    str = String(str);
-    pos = Number(pos) >>> 0;
-
-    text_so_far = str.slice(0, pos);
-    num_words_so_far = text_so_far.split(" ").length;
-
-    return num_words_so_far;
-  }
-
-  function speak_title(speakElement) {
-    bigstring = "";
-    speakElement
-      .find(".title")
-      .find(".word")
-      .each(function (index, item) {
-        console.log(item);
-        bigstring += $(item).text() + " ";
-      });
-
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = bigstring;
-    msg.lang = speech_voice;
-    msg.rate = 0.7;
-    window.speechSynthesis.cancel();
-    window.speechSynthesis.speak(msg);
-
-    msg.onboundary = function (event) {
-      console.log(event.charIndex);
-      console.log(getWordAt(bigstring, event.charIndex));
-
-      word_idx = getWordIndexAtCharIndex(bigstring, event.charIndex);
-      idOfChild = "word_" + word_idx;
-      speakElement.find("#" + idOfChild).css("color", "blue");
-    };
-
-    msg.onend = function (event) {
-      $(".word").css("color", "black");
-      console.log("DONE");
-    };
-  }
-
-  function speak(text) {
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = text;
-    msg.lang = speech_voice;
-    msg.rate = 0.5;
-    window.speechSynthesis.cancel();
-    window.speechSynthesis.speak(msg);
-  }
-
-  function speak_title_helper(event) {
-    div = $(event.target).parent().parent();
-    speak_title(div);
-  }
-
-  function show_partial_translation(event) {
-    $(event.target).parent().parent().find(".title_mix").toggle();
-    position_tooltips();
-  }
-
-  $(".button_partial_translation").on("click", show_partial_translation);
-  $(".button_full_translation").on("click", show_full_translation);
-
-  $(".button_speak").on("click", speak_title_helper);
 
   position_tooltips();
   window.addEventListener("resize", position_tooltips, true);
